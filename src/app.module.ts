d9d69b1369b9fcd0aeadfbc080bf76500c7dbc50
Rostyslav <rostykwave@gmail.com>
2024-07-24 15:34:35 +0300

    feat: add env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigurationModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { CompanyModule } from './company/company.module';
 import { JobModule } from './job/job.module';
 import { AppliancesModule } from './appliances/appliances.module';
 
 @Module({
-  imports: [DatabaseModule, CompanyModule, JobModule, AppliancesModule],
+  imports: [
+    ConfigurationModule,
+    DatabaseModule,
+    CompanyModule,
+    JobModule,
+    AppliancesModule,
+  ],
 })
 export class AppModule {}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,16 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Appliance } from './entities/appliance.entity';
 import { Company } from './entities/company.entity';
 import { Job } from './entities/job.entity';
+console.log('process', process.env.DB_USERNAME);
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'job_portal',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Appliance, Company, Job],
       synchronize: true,
     }),
